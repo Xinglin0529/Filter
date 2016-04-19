3b28d7b015c28d726128dae7c7076bc59e9be453
Dongdong <XUDONGDONG121@pingan.com.cn>
2016-04-19 14:07:51 +0800

    Initial Commit

---
+++ FilterProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FilterProject
+//
+//  Created by Dongdong on 16/4/19.
+//  Copyright © 2016年 DD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
